6512eabed57fb12ee6d07fdf8fdac87ed9805a21
nischay-chauhan <masternischay@outlook.com>
2023-12-21 13:06:00 +0530

    Adding Form using chakra

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { ChakraProvider, CSSReset, Box, Container, VStack, Text } from '@chakra-ui/react';
 import './App.css'; // Import external CSS
+import TaskForm from './component/TaskForm';
 
 
 const App = () => {
   return (
     <ChakraProvider>
       <CSSReset />
+      <Box
+        bgGradient="linear(to-r, purple.500, yellow.500)"
+        minH="80vh"
+        position="relative"
+        overflow="hidden"
+        textAlign="center"
+        >
+        <VStack
+        spacing={8} align="center"
+        >
+        <Text
+          color="white"
+          fontSize="5xl"
+          fontWeight="bold"
+         _hover={{ transform: 'scale(1.1)' }}
+          cursor="pointer"
+          transition="transform 0.3s ease" 
+          mt={8}
+        >
+          To-Do App
+        </Text>
+        <Container maxW={'x'}>
+        <TaskForm />
+        </Container>
+        </VStack>
+      </Box>
     </ChakraProvider>
   );
 };
